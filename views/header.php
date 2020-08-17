186e3c52c8c7d77b526675fbf576a8b07570c8e4
Vy Nguyen <nguy1029@flinders.edu.au>
2020-08-17 10:16:14 +0930

    Initial commit 2

---
+++ views/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Legend Coffee & Tea</title>
+	<link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="public/style/style.css">
+
+</head>
+<body>
+	<div class="header">
+		<div class="menu">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <a class="navbar-brand" href="#"><img src="public/images/legend-coffee-logo.png" width="150"></a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+			    <ul class="navbar-nav">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) ?>">Home <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) ?>/shopping">Shopping</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) ?>/about-us">About Us</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) ?>/contact-us">Contact Us</a>
+			      </li>			      
+			      <!-- <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          Dropdown link
+			        </a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+			          <a class="dropdown-item" href="#">Action</a>
+			          <a class="dropdown-item" href="#">Another action</a>
+			          <a class="dropdown-item" href="#">Something else here</a>
+			        </div>
+			      </li> -->
+
+			    </ul>
+			  </div>
+			  <div>
+			  	<ul class="navbar-nav loginbar">
+			  		<li><a class="login-link" href="">Login</a></li>
+			  		<li>|</li>
+			  		<li><a class="login-link" href="">Sign up</a></li>
+			  	</ul>
+			  </div>
+			</nav>
+		</div>
+	</div>
+	<div class="content">
+		
+	
---
+++ views/home.php
@@ -0,0 +1,2 @@
+	<?php echo "Hello Home";
+	echo dirname($_SERVER['PHP_SELF']); ?>
