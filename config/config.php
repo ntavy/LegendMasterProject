5679c204a33050edeee45777ce2773f9b5e590e7
Vy Nguyen <nguy1029@flinders.edu.au>
2020-08-25 22:17:21 +0930

    connect database, select brand, select supllier, add new product

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'legend_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+define('DB_CHARSET', 'utf8');
