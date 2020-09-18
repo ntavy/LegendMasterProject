dba5433124ab285d1ba80b2ecb8c30f1fdeacc39
deepakbawa22 <bawa0016@flinders.edu.au>
2020-09-18 10:55:22 +0930

    Added back-end workflow to contact-us page

--- controllers/contact-us.php
+++ controllers/contact-us.php
@@ -4,8 +4,21 @@ class ContactUsController extends Controller
 {
     public function index()
     {
-        $this->view('contact-us', ['activeMenu'=>"contact-us"]);
+        $this->view('contact-us', ['activeMenu' => "contact-us"]);
+    }
+
+    public function send()
+    {
+        //print_r($_POST);
+        $this->model('contact');
+        $insert = Contact::addMessage($_POST['firstname'], $_POST['lastname'], $_POST['emailId'], $_POST['company'], $_POST['subject']);
+
+        //return view with message.
+        $this->view('contact-us', [
+            'activeMenu' => "contact-us",
+            'resMessage' => $insert
+        ]);
+
     }
 }
 
-?>
--- views/contact-us.php
+++ views/contact-us.php
@@ -6,7 +6,8 @@
     </div>
     <div class="row">
         <div class="col-sm-6">
-            <img src="public/images/legend-coffee-logo.png" alt="Image" style="width:50%">
+            <img src="<?php echo dirname($_SERVER['PHP_SELF']) ?>/public/images/legend-coffee-logo.png" alt="Image"
+                 style="width:50%">
 
             <ul class="contact-text">
                 <li><b>Address: </b> 123 head st, Adelaide SA 5000</li>
@@ -15,22 +16,39 @@
             </ul>
         </div>
         <div class="col-sm-6">
-            <form action="" method="">
+
+            <?php
+            if (isset($data['resMessage'])) {
+                //success
+                if ($data['resMessage'] == '1') {
+                    echo "<div class=\"alert alert-success\">Thank you for contacting us. Your message has been submitted successfully.</div>";
+                } else { //failed
+                    echo "<div class=\"alert alert-danger\">" . $data['resMessage'] . "</div>";
+                }
+
+                //send page back to contact-us
+                header("refresh:5; url=" . dirname($_SERVER['PHP_SELF']) . "/contact-us");
+            }
+            ?>
+
+            <form action="<?php echo dirname($_SERVER['PHP_SELF']) ?>/contact-us/send" method="POST">
                 <label for="fname">First Name</label>
-                <input class="input-legend" type="text" id="fname" name="firstname" placeholder="First Name">
+                <input class="input-legend" type="text" id="fname" name="firstname" placeholder="First Name"
+                       minlength="2" required>
 
                 <label for="lname">Last Name</label>
-                <input class="input-legend" type="text" id="lname" name="lastname" placeholder="Last Name">
+                <input class="input-legend" type="text" id="lname" name="lastname" placeholder="Last Name" minlength="2"
+                       required>
 
                 <label for="email">Email</label>
-                <input class="input-legend" type="text" id="email" name="emailId" placeholder="Email ID">
+                <input class="input-legend" type="email" id="email" name="emailId" placeholder="Email ID" required>
 
                 <label for="company">Company</label>
                 <input class="input-legend" type="text" id="company" name="company" placeholder="Company Name">
 
                 <label for="subject">Subject</label>
                 <textarea class="input-legend" id="subject" name="subject" placeholder="Write something.."
-                          style="height:170px"></textarea>
+                          style="height:170px" required></textarea>
                 <br><br>
                 <input type="submit" value="Submit" class="btn btn-legend btn-legend-lrg">
             </form>
